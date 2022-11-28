4629a0eb828267ffed7b5929487084aedec89ac5
Morning <morning@admins-iMac-3.local>
2022-11-28 12:33:52 +0800

    js

---
+++ js/basic/function.js
@@ -0,0 +1,7 @@
+let num1 = 15,
+  num2 = 10;
+let type1 = "Addition of given 2 numbers is";
+let type2 = "Subtraction of given 2 numbers is";
+
+console.log(type1, num1 + num2);
+console.log(type2, num1 - num2);
---
+++ js/basic/modul.js
@@ -0,0 +1,5 @@
+let num1 = 66;
+let num2 = (num1 - (num1 % 10)) / 10;
+let num3 = (num2 - (num2 % 10)) / 10;
+
+console.log(num3);
