fe7bc3854bf5ffdbc059ccdccd28d39b1be3f2eb
Morning <morning@admins-iMac-3.local>
2022-11-29 10:19:19 +0800

    html css folder

---
+++ js/basic2/basic.js
@@ -0,0 +1,28 @@
+// let myage = 21;
+// if (myage < 11) {
+//   console.log("child");
+// } else if (myage < 18) {
+//   console.log("teen")
+// } else if (myage < 40) {
+//
+//
+
+let day = 3;
+
+if (day == 1) {
+  console.log("monday");
+} else if (day == 2) {
+  console.log("tuesday");
+} else if (day == 3) {
+  console.log("wednesday");
+} else if (day == 4) {
+  console.log("thursday");
+} else if (day == 5) {
+  console.log("friday");
+} else if (day == 6) {
+  console.log("saturday");
+} else if (day == 7) {
+  console.log("sunday");
+} else {
+  console.log("not found");
+}
