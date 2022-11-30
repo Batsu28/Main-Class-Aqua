c86dbc6956673c18bcc6d5aab1ac3f679e85e36e
Morning <morning@admins-iMac-3.local>
2022-11-30 11:55:58 +0800

    10sariin 30

---
+++ js/basic3/first.js
@@ -0,0 +1,38 @@
+// 1
+
+/*
+let mystring = "sfahfJ";
+
+console.log(mystring.length);
+console.log(mystring.charAt(0));
+console.log(mystring[mystring.length - 1]);
+console.log(mystring.toLocaleUpperCase());
+console.log(mystring.toLowerCase());
+
+let firstname = "djsh",
+  fullname = firstname.concat("", "hask");
+
+console.log(fullname);
+*/
+
+//2
+
+/*
+let babyshark = "baby shark doo-doo doo-doo";
+
+let firstpart = babyshark.substring(0, babyshark.indexOf("ark"));
+let lastpart = babyshark.substring(
+  babyshark.indexOf("ark") + "ark".length,
+  babyshark.length
+);
+
+firstpart = babyshark.substring(0, babyshark.indexOf("ark"));
+lastpart = babyshark.substring(
+  babyshark.indexOf("ark") + "ark".length,
+  babyshark.length
+);
+babyshark = firstpart.concat(lastpart);
+
+console.log(babyshark);
+*/
+console.log(Math.ceil(Math.random() * 6));
