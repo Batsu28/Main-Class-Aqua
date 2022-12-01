f552ce9d83f5443bbf6fbe0d4b88c5a9ae38cfaa
Morning <morning@admins-iMac-3.local>
2022-12-01 12:35:07 +0800

    loop

---
+++ js/basic4/first.js
@@ -0,0 +1,55 @@
+/*
+let lorem =
+  "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+let count = 0;
+let text = 0;
+
+while (count < lorem.length) {
+  if (lorem[count] == "b" || lorem[count] == "B") {
+    text = text + 1;
+  }
+  count = count + 1;
+}
+
+console.log(text);
+*/
+
+//
+
+/*
+let num = 100;
+let count = 0;
+
+while (num > 0) {
+  if (num % 3 == 0) {
+    count = count + 1;
+  }
+  num--;
+}
+
+console.log(count);
+*/
+
+//
+
+/*
+let x = 10,
+  count = 1;
+output = "";
+while (x > 0) {
+  let y = 9;
+  while (y > 0) {
+    if (count % 2 == 0) {
+      output = output + count + "\t";
+      if (count % 18 == 0) output += "\n";
+    }
+    count++;
+    y--;
+  }
+  x--;
+}
+
+console.log(output);
+*/
+
+//
