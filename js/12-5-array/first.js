1408ad39d06612589c31650825a665708d1805f3
Morning <morning@admins-iMac-3.local>
2022-12-05 12:31:23 +0800

    array

---
+++ js/12-5-array/first.js
@@ -0,0 +1,71 @@
+// 1
+// ex: 1
+//    2 3
+//   4 5 6
+
+// let count = 0;
+// let output = "";
+
+// for (let row = 0; row < 4; row++) {
+//   for (let dash = 4 - row; dash > 0; dash--) {
+//     output += " ";
+//   }
+//   for (let col = 0; col <= row; col++) {
+//     count++;
+//     output += count + " ";
+//   }
+//   output += "\n";
+// }
+
+// console.log(output);
+
+//
+
+//
+// Array 1
+//
+
+// let studentAge = [10, 20, 40, 10, 21, 18];
+
+// for (let num = 0; num < studentAge.length; num++) {
+//   console.log(studentAge);
+// }
+
+// 2
+
+// let stuName = ["boldo", "dorjo", "tsetsge", "dulmaa"];
+// let i = 0;
+// while (i <= stuName.length) {
+//   if (stuName[i] && stuName[i][0] === "d") {
+//     console.log(stuName[i]);
+//   }
+//   i++;
+// }
+
+// 3
+
+// let studentName = ["boldo", "dorjo", "tsetsge", "dulmaa"];
+
+// for (let count = 20; count <= 1; count--) {
+//   for (let i = 0; i <= studentName.length; i++) {
+//     if (studentName[i] && studentName[i].length >= count) {
+//       console.log(studentName[i]);
+//     }
+//   }
+//   break;
+// }
+
+//
+// 4
+//
+
+// for (let i = o; j < XMLDocument.length; i++) {
+//   for (let j = 0; j < x[0].length; j++) {}
+// }
+
+//
+// date
+//
+
+let now = new Date();
+console.log(now.getHours());
