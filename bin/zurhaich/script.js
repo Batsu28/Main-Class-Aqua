fad5db9b38d712fdd8bd47b357d43324908412c4
unknown <bataa.banaa@gmail.com>
2022-12-20 21:50:16 +0800

    style

--- bin/zurhaich/script.js
+++ bin/zurhaich/script.js
@@ -1,46 +1,112 @@
+let question = [
+	document.getElementById("question1"),
+	document.getElementById("question2"),
+	document.getElementById("question3"),
+];
 
-let question1 = document.getElementById("question1");
-let question2 = document.getElementById("question2");
-let question3 = document.getElementById("question3");
+let answer = [
+	document.getElementById("answer1"),
+	document.getElementById("answer2"),
+	document.getElementById("answer3"),
+];
 
-let answer1 = document.getElementById("answer1");
-let answer2 = document.getElementById("answer2");
-let answer3 = document.getElementById("answer3");
+let btn = [
+	document.querySelector("#start"),
+	document.querySelector("#enter"),
+	document.querySelector("#reset"),
+];
 
+let gameBg = document.querySelector("#opacityBg");
 let output = document.getElementById("output");
 
-let startBtn = document.querySelector("button")
+let questArr1 = [
+	"өндөр",
+	"намхан",
+	"сахалтай",
+	"балчир(залуу)",
+	"урт үстэй",
+	"сахалтай",
+	"овортой",
+];
+let questArr2 = [];
+let zurhai = [
+	[
+		"ирэх онд санхүү сэхэж орлого нэмэгдэх нь гагцхүү чиний хичээл зүтгэл мэднэ",
+		"",
+	],
+	["", ""],
+	["ирэх онд эрдмийн зарлага гаргах аюултай тул хичээллээ сайн хийнүү", ""],
+	["", ""],
+	[
+		"автобус таксины хойноос гүйж эрүүл мэнд болон дансандаа хөрөнгө оруулалт хийгээрэй",
+		"",
+	],
+	["", "хэмнэж чадвал баяжих нь баттай сэхэх нь баараггүй"],
+	["", ""],
+	[
+		"",
+		"шинэ онд саад бэрхшээлтэй ихэд тулгарач давч чадвал орлогын үүд нээгдэх болно",
+	],
+	["олон хүнтэй найзалж чадвал чиний ирээдгүй гэгээлэг харагдаж байна", ""],
+	["", ""],
+	["", "шинэ онд орлогын үүд нээгдэх тул хичээлээ сайн хийгээрэй"],
+	[
+		"ирэх онд тархи гашлах нигууртай тул тархиа сэргээж бэлтгэлээ базаана уу",
+		"5",
+	],
+];
 
-let gameBg = document.querySelector("#opacityBg")
-
-let questArr1 = ["өндөр", "намхан", "сахалтай"];
-let questArr2 = []
+let answerArr = [
+	answer[0].addEventListener("input", input),
+	answer[1].addEventListener("input", input),
+	answer[2].addEventListener("input", input),
+];
 
 function randomNum(min, max) {
-  return Math.floor(Math.random() * max + min);
+	return Math.floor(Math.random() * (max - min) + min);
 }
 
-startBtn.addEventListener("click",gameStart)
+function input(a) {
+	return a.value;
+}
 
-function gameStart(){
-console.log("test")
+btn[0].addEventListener("click", gameStart);
+btn[2].addEventListener("click", () => {
+	gameBg.style.display = "none";
+});
 
-gameBg.style.display = "flex"
+function gameStart() {
+	btn[2].style.display = "none";
+	gameBg.style.display = "flex";
 
-let text1 = `Танай ангийн хамгийн ${
-    questArr1[randomNum(0, questArr1.length)]
-} нь хэн бэ?`;
+	let text1 = `Танай ангийн хамгийн ${
+		questArr1[randomNum(0, questArr1.length)]
+	} нь хэн бэ?`;
 
-question1.innerText = `${text1}`;
+	question[0].innerText = `${text1}`;
 
-question2.innerText = "Таны очих дуртай газар хаана вэ?";
+	question[1].innerText = "Таны очих дуртай газар хаана вэ?";
 
-question3.innerText = "Таны хэдэн сард төрсөн бэ?";
+	question[2].innerText = "Таны хэдэн сард төрсөн бэ?";
 
+	btn[1].addEventListener("click", () => {
+		document.querySelector(".top").style.display = "none";
+		btn[1].style.display = "none";
+		btn[2].style.display = "block";
+		output.style.display = "block";
 
-// output.innerText = `Ирэх ${randomNum(2023, 20)} онд ${input1(
-//   answer1
-// )} тай хамт ${answer2.value} явах магадлалтай "\n" ${
-//   answer3.value
-// } сард төрсөн бол шингэн зүйл их хэрэглэх нигууртай`;
+		output.innerHTML = `Ирэх ${randomNum(2023, 2026)} онд ${input(
+			answer[0]
+		)} тай хамт ${input(answer[1])} явах магадлалтай <br> ${input(
+			answer[2]
+		)} сард хүмүүс ${zurhaiRandom()} `;
+	});
+}
+
+function zurhaiRandom() {
+	if (randomNum(0, 10) % 2 == 0) {
+		return zurhai[parseInt(input(answer[2])) - 1][1];
+	} else {
+		return zurhai[parseInt(input(answer[2])) - 1][0];
+	}
 }
