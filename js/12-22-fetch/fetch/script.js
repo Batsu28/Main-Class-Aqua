03329e2c1e17248b01d3b774bf97ef4fb7f5590c
Morning <morning@admins-iMac-3.local>
2022-12-23 12:32:31 +0800

    davtlaga

--- js/12-22-fetch/fetch/script.js
+++ js/12-22-fetch/fetch/script.js
@@ -5,15 +5,7 @@ let btn = document.querySelector("button");
 // https://restcountries.com/v3.1/name/${country}
 // https://restcountries.com/v3.1/alpha/${neighbour}
 
-search.addEventListener("input", () => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    getCountry(search.value);
-  });
-});
-
 function renderCountry(country) {
-  console.log(country);
   const html = `<article class="country">
     <img class="country__img" src="${country.flags.svg}" />
     <div class="country__data">
@@ -21,7 +13,9 @@ function renderCountry(country) {
       <h4 class="country__region">${country.region}</h4>
       <p class="country__row"><span>👫</span>${
         country.population < 1_000_000_000
-          ? (country.population / 1_000_000).toFixed(2) + " M"
+          ? country.population < 1_000_000
+            ? (country.population / 1_000).toFixed(2) + " K"
+            : (country.population / 1_000_000).toFixed(2) + " M"
           : (country.population / 1_000_000_000).toFixed(2) + " B"
       }</p>
       <p class="country__row"><span>🗣️</span>${
@@ -30,13 +24,56 @@ function renderCountry(country) {
       <p class="country__row"><span>💰</span>${
         Object.values(country.currencies)[0].name
       } ${Object.values(country.currencies)[0].symbol}</p>
-    </div>
-  </article>`;
+      
+    </div> 
+    </article>`;
   countriesContainer.innerHTML = html;
+  for (var i = 0; i < country.borders.length; i++) {
+    getNeighbour(country.borders[i]);
+  }
 }
 
 function getCountry(country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((res) => res.json())
-    .then((data) => renderCountry(data[0]));
+    .then((data) => renderCountry(data[0]))
+    .then((data) => borderlist(data[0]));
+}
+
+form.addEventListener("input", () => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getCountry(search.value);
+  });
+});
+
+function getNeighbour(neighbour) {
+  fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+    .then((res) => res.json())
+    .then((data) => borderlist(data[0]));
+}
+
+function borderlist(country) {
+  const html1 = `<article class="country neighbour">
+    <img class="country__img" src="${country.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${country.name.common}</h3>
+      <h4 class="country__region">${country.region}</h4>
+      <p class="country__row"><span>👫</span>${
+        country.population < 1_000_000_000
+          ? country.population < 1_000_000
+            ? (country.population / 1_000).toFixed(2) + " K"
+            : (country.population / 1_000_000).toFixed(2) + " M"
+          : (country.population / 1_000_000_000).toFixed(2) + " B"
+      }</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(country.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(country.currencies)[0].name
+      } ${Object.values(country.currencies)[0].symbol}</p>
+      
+    </div> 
+    </article>`;
+  countriesContainer.innerHTML += html1;
 }
